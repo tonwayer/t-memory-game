9af3dd2f2e7f81094579f03cdf86b730b29e6b00
tonwayer <hugtrayvon@gmail.com>
2022-12-10 20:31:03 +0530

    Memory-Game-updates

---
+++ src/App.js
@@ -0,0 +1,119 @@
+import { useState, useEffect } from "react";
+import "./App.css";
+import SingleCard from "./components/SingleCard";
+
+const cardImages = [
+  {
+    src: "/img/helmet-1.png",
+    matched: false,
+  },
+  {
+    src: "/img/potion-1.png",
+    matched: false,
+  },
+  {
+    src: "/img/ring-1.png",
+    matched: false,
+  },
+  {
+    src: "/img/scroll-1.png",
+    matched: false,
+  },
+  {
+    src: "/img/shield-1.png",
+    matched: false,
+  },
+  {
+    src: "/img/sword-1.png",
+    matched: false,
+  },
+];
+
+function App() {
+  const [cards, setCards] = useState([]);
+  const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
+
+  // shuffle cards
+  const shuffleCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(() => Math.random() - 0.5)
+      .map((card) => ({ ...card, id: Math.random() }));
+
+    setCards(shuffledCards);
+    setTurns(0);
+  };
+
+  // handle  a choice
+  const handleChoice = (card) => {
+    // console.log(card);
+    choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
+    // console.log(choiceOne);
+    // console.log(choiceTwo);
+  };
+
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+
+        resetTurn();
+      } else {
+        setTimeout(() => resetTurn(), 1000);
+        // resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  // const checkChoices = () => {
+  //   if (choiceOne === choiceTwo) {
+  //     console.log(choiceOne);
+  //     console.log(choiceTwo);
+  //     console.log("compared");
+  //     resetTurn();
+  //   } else {
+  //     console.log("not compared");
+  //   }
+
+  //   // console.log("compared");
+  // };
+
+  // reset choices & increase turn
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
+  return (
+    <div className="App">
+      <h1>Magic Match</h1>
+      <button onClick={shuffleCards}>New Game</button>
+
+      <div className="card-grid">
+        {cards.map((card) => (
+          <SingleCard
+            card={card}
+            key={card.id}
+            handleChoice={handleChoice}
+            flipped={card === choiceOne || card === choiceTwo || card.matched}
+          />
+        ))}
+      </div>
+
+      {/* <h2> check {choiceOne} </h2> */}
+    </div>
+  );
+}
+
+export default App;
